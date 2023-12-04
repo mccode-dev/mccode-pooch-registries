a3c62758aaeed9b77c03e6a7670194c45b63eea6
Gregory Tucker <gregory.tucker@gmail.com>
2023-12-04 18:11:05 +0100

    [Update] workflow and python registration scripts

--- make_registry_files.py
+++ make_registry_files.py
@@ -1,21 +1,46 @@
-def make_registry(dirs, output, recursive=True):
+
+
+def make_registry(base, dirs, output, recursive=True):
     from pathlib import Path
     from pooch import file_hash
-    pattern = '**/*' if recursive else '*'
-    hashes = {p.as_posix(): file_hash(p) for d in dirs for p in Path(d).glob(pattern) if p.is_file()}
+    pat = '**/*' if recursive else '*'
+    hashes = {p.relative_to(base).as_posix(): file_hash(str(p)) for d in dirs for p in base.joinpath(d).glob(pat) if p.is_file()}
     names = sorted(hashes.keys())
     with open(output, 'w') as outfile:
         for name in names:
             outfile.write(f'{name} {hashes[name]}\n')
 
 
-
-if __name__ == '__main__':
+def one_tag(base):
     registries = {
         'mcstas': ('mcstas-comps',),
         'mcxtrace': ('mcxtrace-comps',),
         'libc': ('common/lib/share', 'mcstas/nlib', 'mxctrace/xlib'),
     }
     for name, dirs in registries.items():
-        make_registry(dirs, f'{name}-registry.txt')
+        make_registry(base, dirs, f'{name}-registry.txt')
 
+
+def all_tags():
+    from pathlib import Path
+    import git
+    repo = git.Repo(Path(__file__).parent, search_parent_directories=False)
+    mccode_dir = Path(__file__).parent.joinpath('mccode')
+    mccode = git.Repo(mccode_dir, search_parent_directories=False)
+
+    tags = [tag for tag in repo.tags if str(tag).startswith('v')]
+    missing = [tag for tag in mccode.tags if str(tag).startswith('v') and tag not in tags]
+    for tag in missing:
+        mccode.git.checkout(tag)
+        one_tag(mccode_dir)
+        repo.git.add('mcstas-registry.txt')
+        repo.git.add('mcxtrace-registry.txt')
+        repo.git.add('libc-registry.txt')
+        repo.index.commit(f'Add {tag} registries')
+        repo.git.tag(tag, message=f'Add {tag} registries')
+
+    repo.remote('origin').push(tags=True)
+
+
+if __name__ == '__main__':
+    all_tags()
